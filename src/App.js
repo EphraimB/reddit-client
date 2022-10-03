0156e7446bdd78f5cbf6257fd895a65cdc1bbe21
Ephraim Becker <emb16@outlook.com>
2022-10-02 21:18:35 -0400

    Got the app bar working.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import SearchAppBar from './components/SearchAppBar/SearchAppBar';
 
 const title = 'React client';
 
 function App() {
   return (
     <div className="App">
-      <header>
-      <h1>{title}</h1>
-      </header>
+      <SearchAppBar />
     </div>
   );
 }
